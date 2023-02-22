ddfce323c01965ad75030dbd5125a8e9b652b017
Daniele Tortora <tortora87@gmail.com>
2023-02-22 21:54:30 +0100

    add types

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -4,4 +4,10 @@
 
 <script setup lang="ts">
 import styles from "./BaseButton.module.css";
+
+type Props = {
+  test: string;
+};
+
+defineProps<Props>();
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
