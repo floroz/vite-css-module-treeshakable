76bc18b5a285b99026dcd93fa1261f51a8452daa
Daniele Tortora <tortora87@gmail.com>
2023-03-01 16:09:16 +0100

    add unbuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,15 @@
 import { fileURLToPath, URL } from "node:url";
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  build: {
-    sourcemap: true,
-    cssCodeSplit: true,
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "MyLib",
-      formats: ["es"],
-      fileName: (format, entry) => `${entry}.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        sourcemap: true,
-        esModule: true,
-        preserveModules: true,
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
+  /**
+   * Build is handled by Unbuild
+   */
 });
